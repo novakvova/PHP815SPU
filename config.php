4ce213d629b2d42517f04fbc7aa37fa6fdb98d90
Volodymyr Novak <novakvova@gmail.com>
2020-11-21 12:11:21 +0200

    Base coreui interface

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+echo "<h2>config.php</h2>\n";
+$db_name="ananas";
---
+++ index1.php
@@ -0,0 +1,29 @@
+
+
+
+
+<?php
+//include "config.php";
+////phpinfo();
+//echo "<h1>Привіт</h1>\n";
+//$a = "23";
+//$str = "Hello";
+//unset($db_name);
+//if ($a === 23) {
+//    echo "a==23\n";
+//} else {
+//    echo "a!=23\n";
+//    $local_var="dsdfsfs";
+//}
+//
+//for ($i = 0; $i < 5; $i++) {
+//    echo "<p>$i FOR</p>\n";
+//}
+//
+////перевірка чи існує змінна
+//if (isset($db_name)) {
+//    echo "<h3>Назва бази даних: $db_name\n</h3>";
+//}
+//
+//echo"<p>Local var $local_var</p>";
+?>
