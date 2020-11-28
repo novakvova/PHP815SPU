68fb19662ea63d604d935d4e235003b7859ec916
Volodymyr Novak <novakvova@gmail.com>
2020-11-28 11:25:57 +0200

    Show list users

--- config.php
+++ config.php
@@ -1,3 +1,7 @@
 <?php
-echo "<h2>config.php</h2>\n";
-$db_name="ananas";
+define('DB_NAME','spu815');
+define('DB_USER','spu815');
+define('DB_PASSWORD','RIPwsWFHBdJJ4m1C');
+define('DB_HOST','localhost');
+define('DB_CHARSET','utf8');
+define('DB_DRIVER','mysql');
--- users.php
+++ users.php
@@ -23,6 +23,11 @@
                         <div class="col-md-12">
                             <div class="card">
                                 <div class="card-body">
+                                    <?php
+                                    include_once "connection_database.php";
+                                    $sql = 'SELECT * FROM tbl_users';
+
+                                    ?>
                                     <table class="table table-responsive-sm table-hover table-outline mb-0">
                                         <thead class="thead-light">
                                         <tr>
@@ -39,45 +44,56 @@
                                         </tr>
                                         </thead>
                                         <tbody>
-                                        <tr>
-                                            <td class="text-center">
-                                                <div class="c-avatar"><img class="c-avatar-img"
-                                                                           src="assets/img/avatars/1.jpg"
-                                                                           alt="user@email.com"><span
-                                                            class="c-avatar-status bg-success"></span></div>
-                                            </td>
-                                            <td>
-                                                <div>Yiorgos Avraamu</div>
-                                                <div class="small text-muted"><span>New</span> | Registered: Jan 1, 2015
-                                                </div>
-                                            </td>
-                                            <td class="text-center">
-                                                <svg class="c-icon c-icon-xl">
-                                                    <use xlink:href="node_modules/@coreui/icons/sprites/flag.svg#cif-us"></use>
-                                                </svg>
-                                            </td>
-                                            <td>
-                                                <div class="clearfix">
-                                                    <div class="float-left"><strong>50%</strong></div>
-                                                    <div class="float-right"><small class="text-muted">Jun 11, 2015 -
-                                                            Jul 10, 2015</small></div>
-                                                </div>
-                                                <div class="progress progress-xs">
-                                                    <div class="progress-bar bg-gradient-success" role="progressbar"
-                                                         style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                         aria-valuemax="100"></div>
-                                                </div>
-                                            </td>
-                                            <td class="text-center">
-                                                <svg class="c-icon c-icon-xl">
-                                                    <use xlink:href="node_modules/@coreui/icons/sprites/brand.svg#cib-cc-mastercard"></use>
-                                                </svg>
-                                            </td>
-                                            <td>
-                                                <div class="small text-muted">Last login</div>
-                                                <strong>10 sec ago</strong>
-                                            </td>
-                                        </tr>
+                                        <?php
+                                        foreach ($dbh->query($sql) as $row)
+                                        {
+                                            echo '
+                                        
+                                            <tr>
+                                                <td class="text-center">
+                                                    <div class="c-avatar">
+                                                        <img class="c-avatar-img"
+                                                             src="'.$row["Image"].'"
+                                                             alt="user@email.com"><span
+                                                                class="c-avatar-status bg-success"></span>
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div>'.$row["Name"].'</div>
+                                                    <div class="small text-muted"><span>New</span> | Registered: Jan 1, 2015
+                                                    </div>
+                                                </td>
+                                                <td class="text-center">
+                                                    <svg class="c-icon c-icon-xl">
+                                                        <use xlink:href="node_modules/@coreui/icons/sprites/flag.svg#cif-us"></use>
+                                                    </svg>
+                                                </td>
+                                                <td>
+                                                    <div class="clearfix">
+                                                        <div class="float-left"><strong>50%</strong></div>
+                                                        <div class="float-right"><small class="text-muted">Jun 11, 2015 -
+                                                                Jul 10, 2015</small></div>
+                                                    </div>
+                                                    <div class="progress progress-xs">
+                                                        <div class="progress-bar bg-gradient-success" role="progressbar"
+                                                             style="width: 50%" aria-valuenow="50" aria-valuemin="0"
+                                                             aria-valuemax="100"></div>
+                                                    </div>
+                                                </td>
+                                                <td class="text-center">
+                                                    <svg class="c-icon c-icon-xl">
+                                                        <use xlink:href="node_modules/@coreui/icons/sprites/brand.svg#cib-cc-mastercard"></use>
+                                                    </svg>
+                                                </td>
+                                                <td>
+                                                    <div class="small text-muted">Last login</div>
+                                                    <strong>10 sec ago</strong>
+                                                </td>
+                                            </tr> ';
+
+                                        }
+                                        ?>
+
 
                                         </tbody>
                                     </table>
